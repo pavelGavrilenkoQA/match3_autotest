0d12137efdf18ac672b474106adfa8c779ce561f
Taisia <Taliunya@yandex.ru>
2023-06-09 15:14:16 +0300

    Added debug_window_page_object

---
+++ main/paje_object/debug_window/debug_window_page_object.py
@@ -0,0 +1,322 @@
+from airtest.core.api import *
+
+
+def debug_icon():
+    return Template(
+        r"debug_icon.png",
+        record_pos=(0.415, 1.004),
+        resolution=(1080, 2340)
+    )
+
+
+def text_panel():
+    return Template(
+        r"panel.png",
+        record_pos=(0.415, 1.004),
+        resolution=(1080, 2340)
+    )
+
+
+def plus_coins():
+    return Template(
+        r"debug_plus.png",
+        record_pos=(-0.031, -0.464),
+        resolution=(1080, 2340)
+    )
+
+
+def plus_stars():
+    return Template(
+        r"debug_plus.png",
+        record_pos=(-0.031, -0.326),
+        resolution=(1080, 2340)
+    )
+
+
+def plus_lives():
+    return Template(
+        r"debug_plus.png",
+        record_pos=(-0.029, -0.187),
+        resolution=(1080, 2340)
+    )
+
+
+def plus_last_competed_level():
+    return Template(
+        r"debug_plus.png",
+        record_pos=(0.421, -0.465),
+        resolution=(1080, 2340)
+    )
+
+
+def plus_level():
+    return Template(
+        r"debug_plus.png",
+        record_pos=(-0.001, 0.476),
+        resolution=(1080, 2340)
+    )
+
+
+def plus_hero():
+    return Template(
+        r"debug_plus.png",
+        record_pos=(0.004, 0.569),
+        resolution=(1080, 2340)
+    )
+
+
+def minus_coins():
+    return Template(
+        r"debug_minus.png",
+        record_pos=(-0.349, -0.467),
+        resolution=(1080, 2340)
+    )
+
+
+def minus_stars():
+    return Template(
+        r"debug_minus.png",
+        record_pos=(-0.352, -0.325),
+        resolution=(1080, 2340)
+    )
+
+
+def minus_lives():
+    return Template(
+        r"debug_minus.png",
+        record_pos=(-0.346, -0.189),
+        resolution=(1080, 2340)
+    )
+
+
+def minus_last_competed_level():
+    return Template(
+        r"debug_minus.png",
+        record_pos=(0.1, -0.464),
+        resolution=(1080, 2340)
+    )
+
+
+def minus_level():
+    return Template(
+        r"debug_minus.png",
+        record_pos=(-0.223, 0.479),
+        resolution=(1080, 2340)
+    )
+
+
+def minus_hero():
+    return Template(
+        r"debug_minus.png",
+        record_pos=(-0.221, 0.568),
+        resolution=(1080, 2340)
+    )
+
+
+def infinity_lives():
+    return Template(
+        r"infinity.png",
+        record_pos=(-0.179, -0.184),
+        resolution=(1080, 2340)
+    )
+
+
+def button_reset_multy_quests():
+    return Template(
+        r"reset.png",
+        record_pos=(-0.284, -0.015),
+        resolution=(1080, 2340)
+    )
+
+
+def button_complete_multy_quests():
+    return Template(
+        r"complete.png",
+        record_pos=(-0.084, -0.013),
+        resolution=(1080, 2340)
+    )
+
+
+def button_reload():
+    return Template(
+        r"reload.png",
+        record_pos=(0.202, -0.369),
+        resolution=(1080, 2340)
+    )
+
+
+def select_top():
+    return Template(
+        r"select.png",
+        record_pos=(0.447, -0.219),
+        resolution=(1080, 2340)
+    )
+
+
+def select_bottom():
+    return Template(
+        r"select.png",
+        record_pos=(0.448, -0.149),
+        resolution=(1080, 2340)
+    )
+
+
+def select_new_ftue():
+    return Template(
+        r"new_ftue.png",
+        record_pos=(0.176, -0.115),
+        resolution=(1080, 2340)
+    )
+
+
+def select_baseline():
+    return Template(
+        r"baseline.png",
+        record_pos=(0.169, -0.101),
+        resolution=(1080, 2340)
+    )
+
+
+def select_variant_a():
+    return Template(
+        r"variant_a.png.png",
+        record_pos=(0.173, -0.05),
+        resolution=(1080, 2340)
+    )
+
+
+def button_test():
+    return Template(
+        r"test.png",
+        record_pos=(0.361, -0.012),
+        resolution=(1080, 2340)
+    )
+
+
+def button_shop_unfinished_purchase():
+    return Template(
+        r"shop.png",
+        record_pos=(0.122, 0.079),
+        resolution=(1080, 2340)
+    )
+
+
+def button_season_event_reset():
+    return Template(
+        r"season.png",
+        record_pos=(0.146, 0.177),
+        resolution=(1080, 2340)
+    )
+
+
+def button_garbage_generate():
+    return Template(
+        r"garbage.png",
+        record_pos=(0.169, 0.273),
+        resolution=(1080, 2340)
+    )
+
+
+def button_chest_ads():
+    return Template(
+        r"chest_ads.png",
+        record_pos=(0.172, 0.363),
+        resolution=(1080, 2340)
+    )
+
+
+def button_play_debug():
+    return Template(
+        r"play.png",
+        record_pos=(0.319, 0.519),
+        resolution=(1080, 2340)
+    )
+
+
+def button_freeze_save():
+    return Template(
+        r"freeze.png",
+        record_pos=(-0.231, 0.698),
+        resolution=(1080, 2340)
+    )
+
+
+def button_reset_save():
+    return Template(
+        r"reset.png",
+        record_pos=(0.277, 0.698),
+        resolution=(1080, 2340)
+    )
+
+
+def button_share():
+    return Template(
+        r"share.png",
+        record_pos=(0.047, 0.787),
+        resolution=(1080, 2340)
+    )
+
+
+def button_debug_deactivate():
+    return Template(
+        r"debug.png",
+        record_pos=(0.012, 0.886),
+        resolution=(1080, 2340)
+    )
+
+
+def button_close_debug():
+    return Template(
+        r"close.png",
+        record_pos=(0.44, -0.777),
+        resolution=(1080, 2340)
+    )
+
+
+def button_close_popup_debug():
+    return Template(
+        r"close.png",
+        record_pos=(0.369, -0.292),
+        resolution=(1080, 2340)
+    )
+
+
+def text_sure():
+    return Template(
+        r"sure.png",
+        record_pos=(0.089, -0.319),
+        resolution=(1080, 2340)
+    )
+
+
+def button_no_popup_debug():
+    return Template(
+        r"no.png",
+        record_pos=(-0.198, 0.193),
+        resolution=(1080, 2340)
+    )
+
+
+def button_yes_popup_debug():
+    return Template(
+        r"yes.png",
+        record_pos=(0.196, 0.194),
+        resolution=(1080, 2340)
+    )
+
+
+def text_alert():
+    return Template(
+        r"alert.png",
+        record_pos=(0.001, -0.325),
+        resolution=(1080, 2340)
+    )
+
+
+def button_ok_debug():
+    return Template(
+        r"ok.png",
+        record_pos=(-0.002, 0.207),
+        resolution=(1080, 2340)
+    )
+
