ad84f8bba8c931de8ac5d1d0eecf9248b65a2153
Taisia <Taliunya@yandex.ru>
2023-06-13 14:11:01 +0300

    Added main_settings_page_object

---
+++ main/paje_object/main_settings/main_settings_page_object.py
@@ -0,0 +1,154 @@
+from airtest.core.api import *
+
+
+def text_settings():
+    return Template(
+        r"settings.png",
+        record_pos=(0.0, -0.509),
+        resolution=(1080, 2340)
+    )
+
+
+def button_connect():
+    return Template(
+        r"connect.png",
+        record_pos=(-0.281, 0.183),
+        resolution=(1080, 2340)
+    )
+
+
+def button_privacy_policy():
+    return Template(
+        r"privacy.png",
+        record_pos=(-0.202, 0.531),
+        resolution=(1080, 2340)
+    )
+
+
+def button_restore_purchases():
+    return Template(
+        r"restore.png",
+        record_pos=(-0.055, 0.44),
+        resolution=(1080, 2340)
+    )
+
+
+def button_terms_of_service():
+    return Template(
+        r"terms.png",
+        record_pos=(0.116, 0.531),
+        resolution=(1080, 2340)
+    )
+
+
+def button_close_main_settings():
+    return Template(
+        r"close.png",
+        record_pos=(0.367, -0.483),
+        resolution=(1080, 2340)
+    )
+
+
+def button_close_popup_main_settings():
+    return Template(
+        r"close.png",
+        record_pos=(0.366, -0.31),
+        resolution=(1080, 2340)
+    )
+
+
+def switcher_off_notifications():
+    return Template(
+        r"switcher_off.png",
+        record_pos=(0.244, -0.088),
+        resolution=(1080, 2340)
+    )
+
+
+def switcher_on_notifications():
+    return Template(
+        r"switcher_on.png",
+        record_pos=(0.247, -0.087),
+        resolution=(1080, 2340)
+    )
+
+
+def switcher_off_vibration():
+    return Template(
+        r"switcher_off.png",
+        record_pos=(0.243, 0.019),
+        resolution=(1080, 2340)
+    )
+
+
+def switcher_on_vibration():
+    return Template(
+        r"switcher_on.png",
+        record_pos=(0.246, 0.022),
+        resolution=(1080, 2340)
+    )
+
+
+def english_flag_main_chosen():
+    return Template(
+        r"english_main.png",
+        record_pos=(0.095, 0.181),
+        resolution=(1080, 2340)
+    )
+
+
+def text_languages():
+    return Template(
+        r"languages.png",
+        record_pos=(0.003, -0.338),
+        resolution=(1080, 2340)
+    )
+
+
+def english_flag_popup():
+    return Template(
+        r"english.png",
+        record_pos=(-0.285, -0.116),
+        resolution=(1080, 2340)
+    )
+
+
+def romanian_flag_popup():
+    return Template(
+        r"romanian.png",
+        record_pos=(-0.286, 0.042),
+        resolution=(1080, 2340)
+    )
+
+
+def french_flag_popup():
+    return Template(
+        r"french.png",
+        record_pos=(0.076, 0.2),
+        resolution=(1080, 2340)
+    )
+
+
+def russian_flag_popup():
+    return Template(
+        r"russian.png",
+        record_pos=(0.075, -0.113),
+        resolution=(1080, 2340)
+    )
+
+
+def turkish_flag_popup():
+    return Template(
+        r"turkish.png",
+        record_pos=(-0.284, 0.198),
+        resolution=(1080, 2340)
+    )
+
+
+def ukrainian_flag_popup():
+    return Template(
+        r"ukrainian.png",
+        record_pos=(0.077, 0.042),
+        resolution=(1080, 2340)
+    )
+
