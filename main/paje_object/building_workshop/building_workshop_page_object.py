1f40eec96892fe4b58f0557d9781f65bcdac8ecd
Taisia <Taliunya@yandex.ru>
2023-07-17 16:43:32 +0300

    Started building_workshop_page_object

---
+++ main/paje_object/building_workshop/building_workshop_page_object.py
@@ -0,0 +1,179 @@
+from airtest.core.api import *
+
+
+def count_of_stars():
+    return Template(
+        r"count_of_stars.png",
+        record_pos=(-0.143, -0.948),
+        resolution=(1080, 2400)
+    )
+
+
+def count_of_coins():
+    return Template(
+        r"count_of_coins.png",
+        record_pos=(0.145, -0.949),
+        resolution=(1080, 2400)
+    )
+
+
+def text_workshop():
+    return Template(
+        r"workshop.png",
+        record_pos=(0.0, -0.815),
+        resolution=(1080, 2400)
+    )
+
+
+def button_buildings():
+    return Template(
+        r"buildings.png",
+        record_pos=(-0.18, -0.68),
+        resolution=(1080, 2400)
+    )
+
+
+def button_decorations():
+    return Template(
+        r"decorations.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def button_close():
+    return Template(
+        r"close.png",
+        record_pos=(0.004, -0.46),
+        resolution=(1080, 2400)
+    )
+
+
+def text_castle():
+    return Template(
+        r"castle.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def building_castle():
+    return Template(
+        r"building_castle.png",
+        record_pos=(-0.323, -0.479),
+        resolution=(1080, 2400)
+    )
+
+00000
+def text_small_house():
+    return Template(
+        r"small_house.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def building_small_house():
+    return Template(
+        r"building_small_house.png",
+        record_pos=(-0.323, -0.479),
+        resolution=(1080, 2400)
+    )
+
+
+def text_bakery():
+    return Template(
+        r"bakery.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def building_bakery():
+    return Template(
+        r"building_bakery.png",
+        record_pos=(-0.323, -0.479),
+        resolution=(1080, 2400)
+    )
+
+
+def text_warehouse():
+    return Template(
+        r"warehouse.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def building_warehouse():
+    return Template(
+        r"building_warehouse.png",
+        record_pos=(-0.323, -0.479),
+        resolution=(1080, 2400)
+    )
+
+
+def text_big_house():
+    return Template(
+        r"bakery.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def building_big_house():
+    return Template(
+        r"building_big_house.png",
+        record_pos=(-0.323, -0.479),
+        resolution=(1080, 2400)
+    )
+
+
+def text_huge_house():
+    return Template(
+        r"huge_house.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def building_huge_house():
+    return Template(
+        r"building_huge_house.png",
+        record_pos=(-0.323, -0.479),
+        resolution=(1080, 2400)
+    )
+
+
+def text_juicer():
+    return Template(
+        r"juicer.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def building_juicer():
+    return Template(
+        r"building_juicer.png",
+        record_pos=(-0.323, -0.479),
+        resolution=(1080, 2400)
+    )
+
+
+def text_ice_cream_maker():
+    return Template(
+        r"ice_cream_maker.png",
+        record_pos=(-0.322, -0.336),
+        resolution=(1080, 2400)
+    )
+
+
+def building_ice_cream_maker():
+    return Template(
+        r"building_ice_cream_maker.png",
+        record_pos=(-0.323, -0.479),
+        resolution=(1080, 2400)
+    )
+
+
